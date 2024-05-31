70afb88433ee4fb2e0bfdecddad2a6c6d84708a8
NicolasAgustinIbarra <nicoibarra099@gmail.com>
2024-05-31 13:59:28 -0300

    trabajando en el front, problemas al recuperar el total a repartir de los montos

--- src/components/vender/components/remito/header-2/payment-method-afip-information/modal-payment-methods/Modal.vue
+++ src/components/vender/components/remito/header-2/payment-method-afip-information/modal-payment-methods/Modal.vue
@@ -1,49 +1,81 @@
 <template>
 	<b-modal 
-	id="payment-method-modal" 
-	title="Métodos de pago" 
-	hide-footer>
-
-	<!-- 
-		Total a repartir
-		Boton "completar" en cada input
-		Almacenar los metodos de pago y sus montos en un array
-		para enviar al api
-	 -->
-		
-		<b-form-group
-		v-for="payment_method in payment_methods"
+		id="payment-method-modal" 
+		title="Métodos de pago" 
+		hide-footer>
+	
+		<!-- Total a repartir -->
+	<label class="total-a-repartir">Total a repartir: ${{ total_a_repartir }}</label>
+		<!-- Lista de métodos de pago -->
+	<b-form-group
+		v-for="(payment_method, index) in payment_methods"
+		:key="payment_method.id"
 		:label="payment_method.name">
-			<b-form-input
-			placeholder="Ingrese el monto"></b-form-input>
+			<div class="d-flex">
+				<b-form-input
+					v-model="metodos_de_pago_seleccionados[payment_method.id]"
+					@input="updateTotal(index)"
+					placeholder="Ingrese el monto">
+				</b-form-input>
+				<b-button 
+					variant="outline-primary" 
+					class="ml-2 p-2 btn-total" 
+					size="sm"
+					@click="agregarTotal(index, payment_method.id)">
+					Agregar total
+				</b-button>
+			</div>  
 			<hr>
-		</b-form-group>
-
-		<b-button
+	</b-form-group>
+		
+		<!-- Botón para guardar -->
+	<b-button
+		@click="guardarMetodosPago"
 		block
 		variant="primary">
-			Guardar
-		</b-button>
-
+		Guardar
+	</b-button>
+	
 	</b-modal>
 </template>
+	
 <script>
 export default {
 	mounted() {
-		this.total_a_repartir = this.total_vender
+		this.total_a_repartir = this.total_vender;
 	},
 	computed: {
 		payment_methods() {
-			return this.$store.state.current_acount_payment_method.models 
+			return this.$store.state.current_acount_payment_method.models
 		},
 		total_vender() {
-			return this.$store.state.vender.total 
+			return this.$store.state.vender.total;
 		},
 	},
 	data() {
 		return {
 			total_a_repartir: 0,
+			metodos_de_pago_seleccionados: [],
+		}
+	},
+	methods: {
+			updateTotal(index) {
+				this.total_a_repartir -= this.metodos_de_pago_seleccionados[index].value;
+			},
+			
+		guardarMetodosPago() {		
+				console.log(this.metodos_de_pago_seleccionados)
+			}
 		}
 	}
-}
+	
 </script>
+	
+<style>
+	.total-a-repartir{
+		font-size: 20px;
+	}
+	.btn-total {
+		white-space: nowrap;
+	}
+</style>  
