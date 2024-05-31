a2d316168ba181a060b635c5df93feaba5c2eed6
lucas <lucasgonzalez5500@gmail.com>
2024-05-31 09:39:59 -0300

    Correcciones

--- src/components/vender/components/remito/header-2/payment-method-afip-information/PaymentMethod.vue
+++ src/components/vender/components/remito/header-2/payment-method-afip-information/PaymentMethod.vue
@@ -1,32 +1,30 @@
 <template>
-	<div>
-	  <b-form-select
-		v-model="current_acount_payment_method_id"
-		:options="getOptions({key: 'current_acount_payment_method_id', text: 'Metodo de pago'})">
-	  </b-form-select>
-	  <b-button class="btn btn-outline-primary mb-2" variant="outline-primary" @click="showModal"><i class="icon-plus"></i>Agregar métodos de pago</b-button>
+<div>
+	<b-form-select
+	v-model="current_acount_payment_method_id"
+	:options="getOptions({key: 'current_acount_payment_method_id', text: 'Metodo de pago'})">
+	</b-form-select>
+		<b-button 
+		class="mb-2" 
+		variant="outline-primary" 
+		v-b-modal="'payment-method-modal'">
+			<i class="icon-plus"></i>
+			Agregar métodos de pago
+		</b-button>
 
-	  <PaymentMethodModal ref="paymentMethodModal"></PaymentMethodModal>
-	</div>
-  </template>
-  
-  <script>
-  	import vender from '@/mixins/vender'
-	import PaymentMethodModal from './Modal/PaymentMethodModal.vue';
-
-  
-  export default {
+	<PaymentMethodModal ref="paymentMethodModal"></PaymentMethodModal>
+</div>
+</template>
+<script>
+import vender from '@/mixins/vender'
+export default {
 	mixins: [vender],
 	components: {
-		PaymentMethodModal,	
+		PaymentMethodModal: () => import('@/components/vender/components/remito/header-2/payment-method-afip-information/modal-payment-methods/Modal')	
 	},
 	methods: {
-	  showModal() {
-		this.$refs.paymentMethodModal.$bvModal.show('payment-method-modal');
-	  }
 	},
-
-  }
+}
   </script>
   
   
---
+++ src/components/vender/components/remito/header-2/payment-method-afip-information/modal-payment-methods/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+	<b-modal 
+	id="payment-method-modal" 
+	title="Métodos de pago" 
+	hide-footer>
+
+	<!-- 
+		Total a repartir
+		Boton "completar" en cada input
+		Almacenar los metodos de pago y sus montos en un array
+		para enviar al api
+	 -->
+		
+		<b-form-group
+		v-for="payment_method in payment_methods"
+		:label="payment_method.name">
+			<b-form-input
+			placeholder="Ingrese el monto"></b-form-input>
+			<hr>
+		</b-form-group>
+
+		<b-button
+		block
+		variant="primary">
+			Guardar
+		</b-button>
+
+	</b-modal>
+</template>
+<script>
+export default {
+	mounted() {
+		this.total_a_repartir = this.total_vender
+	},
+	computed: {
+		payment_methods() {
+			return this.$store.state.current_acount_payment_method.models 
+		},
+		total_vender() {
+			return this.$store.state.vender.total 
+		},
+	},
+	data() {
+		return {
+			total_a_repartir: 0,
+		}
+	}
+}
+</script>
