a0b70dcf29ce59c70cf80ccce39e9d24d52a4c5c
Lucas <lucasgonzalez5500@gmail.com>
2026-02-13 14:32:29 -0300

    .

---
+++ src/components/listado/components/modal-props/PriceInput.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<b-form-input
+		:disabled="disabled"
+		v-model="article.price"
+		placeholder="Precio manual"></b-form-input>
+		<p
+		class="m-t-10"
+		v-if="disabled">
+			Elimine el margen de ganancia para poder fijar el precio manualmente
+		</p>
+	</div>
+</template>
+<script>
+export default {
+	computed: {
+		article() {
+			return this.$store.state.article.model 
+		},
+		disabled() {
+			return this.article && this.article.percentage_gain !== null && this.article.percentage_gain != ''
+		}
+	},
+	methods: {
+		info() {
+			this.$api.get('article/final-price-description/'+this.article.id)
+			.then(res => {
+				console.log(res)
+
+				this.$store.commit('article/set_final_price_description', res.data.description)
+				this.$bvModal.show('final-price-description')
+			})
+		}
+	}
+}
+</script>
---
+++ src/components/listado/modals/FinalPriceDescription.vue
@@ -0,0 +1,32 @@
+<template>
+	<b-modal
+	title="Calculo del precio"
+	hide-footer
+	size="md"
+	id="final-price-description">
+		<div
+		v-for="des in final_price_description">
+			<div
+			v-if="des === des.toUpperCase()">
+				<hr>
+				<h4>
+					{{ des }}
+				</h4>
+				<hr>
+			</div>
+			<p
+			v-else>
+				{{ des }}
+			</p>
+		</div>
+	</b-modal>
+</template>
+<script>
+export default {
+	computed: {
+		final_price_description() {
+			return this.$store.state.article.final_price_description
+		},
+	},
+}
+</script>
