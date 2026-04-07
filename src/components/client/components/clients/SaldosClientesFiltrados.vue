940c0c37328eee520bdda4699fd9e6a9c2188399
Lucas <lucasgonzalez5500@gmail.com>
2026-04-07 16:24:26 -0300

    .

--- src/components/client/components/clients/SaldosClientesFiltrados.vue
+++ src/components/client/components/clients/SaldosClientesFiltrados.vue
@@ -1,9 +1,16 @@
 <template>
-	<p
-	class="m-t-15 m-b-0 m-l-15"
-	v-if="filtered.length">
-		Suma de los saldos: <strong>{{ suma_saldos }}</strong>
-	</p>
+	<div>
+		<p
+		class="m-t-15 m-b-0 m-l-15"
+		v-if="filtered.length">
+			Suma de los saldos en pesos: <strong>{{ suma_saldos_pesos }}</strong>
+		</p>
+		<p
+		class="m-t-15 m-b-0 m-l-15"
+		v-if="filtered.length && hasExtencion('ventas_en_dolares')">
+			Suma de los saldos en dolares: <strong>{{ suma_saldos_dolares }}</strong>
+		</p>
+	</div>
 </template>
 <script>
 export default {
@@ -11,13 +18,20 @@ export default {
 		filtered() {
 			return this.$store.state.client.filtered
 		},
-		suma_saldos() {
+		suma_saldos_pesos() {
 			let total = 0
 			this.filtered.forEach(client => {
-				total += Number(client.saldo)
+				total += Number(client.saldo_pesos)
 			})
 			return this.price(total)
-		}
+		},
+		suma_saldos_dolares() {
+			let total = 0
+			this.filtered.forEach(client => {
+				total += Number(client.saldo_dolares)
+			})
+			return this.price(total)
+		},
 	}
 }
 </script>
