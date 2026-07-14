3a1101874f5a59b97ade4fcac1e5e6c5fc300b78
Lucas <lucasgonzalez5500@gmail.com>
2026-07-14 17:04:48 -0300

    feat: tabla de stock minimo paginada con resumen y refresco por broadcast (prompt 399)

--- src/components/common/ArticlesStockMinimo.vue
+++ src/components/common/ArticlesStockMinimo.vue
@@ -1,10 +1,12 @@
 <template>
 	<b-modal
-	size="lg"
+	size="xl"
 	hide-footer
+	lazy
 	title="Alerta de stock minimo"
-	id="articles-stock-minimo">
-		
+	id="articles-stock-minimo"
+	@hidden="resetear_filtro_stock_minimo">
+
     	<list-articles></list-articles>
 
 	</b-modal>
@@ -14,6 +16,18 @@
 export default {
 	components: {
 		ListArticles: () => import('@/components/alertas/components/lista-de-alertas-table/stock-minimo/ListArticles'),
+	},
+	methods: {
+		/**
+		 * Al cerrar el modal, resetea busqueda y pagina en el store para que la proxima vez
+		 * que se abra arranque limpio, sin el filtro de la sesion anterior aplicado.
+		 * Como el modal usa `lazy`, el `list-articles` se destruye al cerrarlo y se vuelve a
+		 * crear (con `created()` propio) la proxima vez que se abre, ya con estos valores reseteados.
+		 */
+		resetear_filtro_stock_minimo() {
+			this.$store.commit('inventory_performance/set_search', '')
+			this.$store.commit('inventory_performance/set_page', 1)
+		},
 	},
 }
 </script>
